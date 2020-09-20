cded3ddf4303e57b54002ca6c3a86dec884d1f53
Matheus Martinez <matheus.rmartinez@gmail.com>
2020-09-20 19:31:25 -0300

    API Node - Organização dos imports - Criação de método para validar os projetos - Criação de método que cria um log para saber quanto tempo a requisição levou

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 const projects = [];
 
 const app = express();
 app.use(express.json());
+app.use(logRequests);
+app.use("/projects/:id", validateProjectId);
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}]${url}`;
+  console.log(logLabel);
+
+  next();
+
+  console.timeEnd();
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Project ID not found." });
+  }
+  return next();
+}
 
 app.get("/projects", (request, response) => {
   const { title } = request.query;
@@ -43,11 +64,6 @@ app.put("/projects/:id", (request, response) => {
 app.delete("/projects/:id", (request, response) => {
   const { id } = request.params;
   const projectIndex = projects.findIndex((project) => project.id === id);
-
-  if (projectIndex < 0) {
-    return response.status(400).json({ error: "Project not found." });
-  }
-
   projects.splice(projectIndex, 1);
 
   return response.status(204).send();
