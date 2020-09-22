13e254954138bed3d272be989877c76e983a7e75
Matheus Martinez <matheus.rmartinez@gmail.com>
2020-09-22 07:50:12 -0300

    Adicionado pacote cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const { uuid, isUuid } = require("uuidv4");
 const projects = [];
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(logRequests);
 app.use("/projects/:id", validateProjectId);
